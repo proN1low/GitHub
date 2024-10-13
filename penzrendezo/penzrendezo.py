13d344ad45bf97ad54b5f9d14a987edd927663f1
Gergő Bojczán <46872308+proN1low@users.noreply.github.com>
2024-10-13 15:10:21 +0200

    Create penzrendezo.py

---
+++ penzrendezo/penzrendezo.py
@@ -0,0 +1,10 @@
+szaz = int(input("Százas (db): ")) * 100
+osszeg = szaz
+
+ketszaz = int(input("Kétszázas (db): ")) * 200
+osszeg += ketszaz
+
+otszaz = int(input("Ötszázas (db): ")) * 500
+osszeg += otszaz
+
+print(f'{osszeg} Ft a beírt pénz összege.')
