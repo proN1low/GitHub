195a18354a8ac7ccd3e78356ed652b71e8965c4b
Gergő Bojczán <46872308+proN1low@users.noreply.github.com>
2024-10-16 21:54:38 +0200

    Create szorzat.py

---
+++ szorzat/szorzat.py
@@ -0,0 +1,8 @@
+print("Egy függvényt tesztelünk, amely két szám szorzatát adja vissza eredményül.")
+
+def szorzat():
+    elso = float(input("Adj meg egy számot: "))
+    masodik = float(input("Adj meg még egy számot: "))
+    szorzat=elso*masodik
+    print(f'A két szám szorzata: {szorzat}')
+szorzat()
